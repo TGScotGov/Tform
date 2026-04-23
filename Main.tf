ed3191434516ce1f844310646a94ac216faaf27f
TGScotGov <wisbeech@gmail.com>
2026-04-23 16:56:00 +0100

    Terraform simple vNet creation

---
+++ Main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "1-6f60cb81-playground-sandbox"
+  location = "southcentraluscls"
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "wisbeech-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
